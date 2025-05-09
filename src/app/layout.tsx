3256c4893fa5fe75dbaba34eaaa1bd2ed388ec32
Marina2022 <marusiiiia@yandex.ru>
2025-05-09 09:17:03 +0300

    initial

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import "./globals.scss";
+import "@/assets/styles/fonts.scss";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
